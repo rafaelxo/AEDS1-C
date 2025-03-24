1df7a323abc469bd588fcd6aff651911c63605f7
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-03-24 16:44:02 -0300

    8

--- lista02_estruturasrepeticao/exercicio3.c
+++ lista02_estruturasrepeticao/exercicio3.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main() {
-    float num;
     int d39 = 0, d2 = 0, d5 = 0;
     for (int cont1 = 1; cont1 <= 10; cont1++) {
+        float num;
         scanf("%f", &num);
-        if ((num / 3 == (int)(num / 3)) && (num / 9 == (int)(num / 9))) {
+        if (fmod (num, 3) == 0 && fmod (num, 9) == 0) {
             d39++;
         } 
-        if (num / 2 == (int)(num / 2)) {
+        if (fmod (num, 2) == 0) {
             d2++;
         }
-        if (num / 5 == (int)(num / 5)) {
+        if (fmod (num, 5) == 0) {
             d5++;
         }
-        if (!((num / 3 == (int)(num / 3)) && (num / 9 == (int)(num / 9)) || num / 2 == (int)(num / 2) || num / 5 == (int)(num / 5))) {
+        if (!(fmod (num, 3) == 0 && fmod (num, 9) == 0 || fmod (num, 2) == 0 || fmod (num, 5) == 0)) {
             printf("Numero nao e divisivel pelos valores\n");
         }
     }
