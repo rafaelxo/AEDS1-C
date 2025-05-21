5e98c74bfda24a70184d13088c5494b5fa877a6a
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-05-21 08:52:37 -0300

    att

--- lista06_vetoresematrizes/exercicio10.c
+++ lista06_vetoresematrizes/exercicio10.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 int main () {
     char *texto = (char*)malloc(100*sizeof(char));
@@ -10,12 +11,10 @@ int main () {
     int vogais = 0, consoantes = 0;
     for (int i = 0; i < tam; i++) {
         letra = *(texto + i);
-        while (letra != ' ' && letra != '\0') {
-            if (letra == 'a' || letra == 'A' || letra == 'e' || letra == 'E' || letra == 'i' || letra == 'I' || letra == 'o' || letra == 'U' || letra == 'u' || letra == 'U') vogais++;
-            else if (letra >= 'a' && letra <= 'z') consoantes++;
-            letra = '\0';
-        }
+        letra = tolower(letra);
+        if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u') vogais++;
+        else if (letra >= 'a' && letra <= 'z') consoantes++;
     }
-    printf("Vogais: %d\nConsoantes: %d\n", vogais, consoantes - vogais);
+    printf("Vogais: %d\nConsoantes: %d\n", vogais, consoantes);
     free(texto);
 }
