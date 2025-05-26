3129eb96728c1d71d5277415660e03923390a2a6
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-05-26 09:32:18 -0300

    att

--- lista06_vetoresematrizes/exercicio12.c
+++ lista06_vetoresematrizes/exercicio12.c
@@ -23,7 +23,7 @@ int main () {
     char *texto = (char*)malloc(100*sizeof(char));
     scanf(" %[^\n]", texto);
     int tam = strlen(texto);
-    printf("As permutacoes da string sao:\n");
+    printf("As permutações da string são:\n");
     permutacao(texto, 0, tam - 1);
     printf("\n");
     free(texto);
